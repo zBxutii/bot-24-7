5911d42c14ecd80b8b64943446c770d5b01152d9
zBxutii <95422142+zBxutii@users.noreply.github.com>
2021-12-11 20:02:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+import keep_alive
+from discord.ext import commands
+
+client = discord.Bot(command_prefix='.')
+
+@client.event
+async def on_ready():
+  print('Bot Encendido.')
+  
+keep_alive.keep_alive
+client.run('token de tu bot')
